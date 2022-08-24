d88328385c4ca2257f70b92b985865050bf84220
ituitis20-arabaci19 <arabaci19@itu.edu.tr>
2022-08-24 19:50:12 +0300

    day 5

---
+++ AlgorithmStudyPlanLeetCode/day5/MiddleoftheLinkedList.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* p = head;
+        ListNode* mid = head;
+        int currentIdx = 0;
+        int midIdx = 0;
+        while(p->next != nullptr)
+        {
+            p = p->next;
+            currentIdx++;
+            if(currentIdx/2 > midIdx)
+            {
+                midIdx++;
+                mid = mid->next;
+            }
+        }
+        if(currentIdx%2 == 1)
+            mid = mid->next;
+        return mid;
+    }
+};
