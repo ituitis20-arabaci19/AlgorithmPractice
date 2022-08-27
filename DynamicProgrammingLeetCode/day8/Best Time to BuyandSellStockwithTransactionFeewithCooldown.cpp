82a8aaa09414c05da0c0c45a8c886cbeda874446
ituitis20-arabaci19 <arabaci19@itu.edu.tr>
2022-08-28 02:46:38 +0300

    Update Best Time to BuyandSellStockwithTransactionFeewithCooldown.cpp

--- DynamicProgrammingLeetCode/day8/Best Time to BuyandSellStockwithTransactionFeewithCooldown.cpp
+++ DynamicProgrammingLeetCode/day8/Best Time to BuyandSellStockwithTransactionFeewithCooldown.cpp
@@ -6,46 +6,56 @@
 using namespace std;
 class Solution {
 public:
-    int maxProfit(vector<int> prices) {
-        bool sell = true;
-        int profit = 0, lastBuy = prices[0], lastSold = INT_MIN;
-        for(int i=1; i<prices.size(); i++)
+    int maxProfit(vector<int>& prices) {
+        unordered_map<int,vector<int>>states; //day -> 0:buy, 1: sell, 2:cooldown, 3: no buy pass, 4:buy pass
+        int res = max(act(states, prices, 0, 3, false), act(states, prices, 0, 0, false));
+        return res;
+    }
+    int act(unordered_map<int,vector<int>>& states, vector<int>& prices, int day, int state, bool lastBuy)
+    {
+        if(day == prices.size())
+            return 0;
+        
+        if(states[day].size() != 0)
         {
-            int price = prices[i];
-            if(sell)
+            if(states[day][state] != -1)
             {
-                if(price <= lastBuy)
-                    lastBuy = price;
-                else
-                {
-                    if(i == prices.size()-1)
-                    {
-                        lastSold = max(lastSold, price);
-                        profit += max(0,lastSold-lastBuy);
-                    }
-                    else if(price > prices[i+1])
-                    {
-                        int curr = maxProfit(vector<int>(prices.begin()+i+1,prices.end()));
-                        int next = maxProfit(vector<int>(prices.begin()+i+2,prices.end()));
-                        profit += max(curr+prices[i-1]-lastBuy, next+price-lastBuy);
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                lastBuy = price;
-                sell = true;
-                lastSold = INT_MIN;
+                return states[day][state];
             }
         }
-        return profit;
+        else
+            states[day] = {-1,-1,-1,-1,-1};
+        //buy
+        if(state == 0)
+        {    
+            lastBuy = true;
+            states[day][state] = max(act(states, prices, day+1, 1, lastBuy), act(states, prices, day+1, 4, lastBuy))-prices[day]; 
+        }
+        //sell
+        else if(state == 1)
+        {
+            states[day][state] = act(states, prices, day+1, 2, false)+prices[day];
+        }
+        else if(state == 2) //cooldown
+        {    
+            states[day][state] = max(act(states, prices, day+1, 0, lastBuy), act(states, prices, day+1, 3, lastBuy));
+        }
+        else //no buy pass or buy pass
+        {    
+            if(lastBuy == true)
+                states[day][state] = max(act(states, prices, day+1, 1, lastBuy), act(states, prices, day+1, 4, lastBuy));
+            else
+                states[day][state] = max(act(states, prices, day+1, 0, lastBuy), act(states, prices, day+1, 3, lastBuy));
+        }
+        return max(states[day][state],0);   
     }
 };
+
+
 int main()
 {
     Solution sol = Solution();
-    vector<int> arr = {4868,4579,2307,3952,3544,651,1300,218,489};
-    int counter = sol.maxProfit(arr,655);
+    vector<int> arr = {6,1,3,2,4,7};
+    int counter = sol.maxProfit(arr);
     cout << counter << endl;
 }
