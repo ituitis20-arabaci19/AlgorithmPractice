5fc985c3f8772cf972af574c399a0c7ce27e01fa
ituitis20-arabaci19 <arabaci19@itu.edu.tr>
2022-08-31 19:04:21 +0300

    day12

---
+++ AlgorithmStudyPlanLeetCode/day12/ClimbingStairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>dp = {1};
+        int counter = takeStep(n, dp);
+        return counter;
+    }
+    int takeStep(int n, vector<int>&dp)
+    {
+        int counter = 0;
+        if(n == 1)
+            return 1;
+        if(n <= 0)
+            return 0;
+        if(n == 2)
+            counter++;
+        if(n <= dp.size())
+            return dp[n-1];
+        counter += takeStep(n-1, dp) + takeStep(n-2, dp);
+        dp.push_back(counter);
+        return counter;
+    }
+};
---
+++ AlgorithmStudyPlanLeetCode/day12/HouseRobber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        map<int,int> dp;
+        return robHouse(nums.size(), nums, dp);
+    }
+    int robHouse(int n, vector<int>& nums, map<int,int>& dp)
+    {
+        if(n <= 0)
+            return 0;
+        int counter = 0;
+        int temp = 0;
+        while(n>0)
+        {
+            if(n < dp.size())
+                temp = dp[n];
+            else
+            {   
+                temp = nums[nums.size()-n] + robHouse(n-2, nums, dp);
+                dp[n] = temp;
+            }
+            counter = counter > temp ? counter : temp;
+            n--;
+        }
+        return counter;
+    }
+};
